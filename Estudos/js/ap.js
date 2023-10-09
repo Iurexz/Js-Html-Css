ea8333cb22e4998af51d695c5b9b0c34f352ba1f
Iure Costa <131163902+Iurexz@users.noreply.github.com>
2023-10-08 21:21:48 -0300

    Update ap.js

--- Estudos/js/ap.js
+++ Estudos/js/ap.js
@@ -4,3 +4,18 @@ const pinto = document.querySelector("#pinto")
 console.log(pinto);
 //
 pinto.textContent = "Meu pinto ficou dulu";
+//////
+
+
+const div_data=document.getElementById("div_data")
+const data=new Date()
+
+let dia=data.getDate()
+dia=dia<10?"0"+dia:dia
+
+let mes=data.getMonth()
+mes=mes<10?"0"+mes:mes
+
+const data_r=dia+"/"+mes+"/"+data.getFullYear()
+
+div_data.innerHTML=data_r
